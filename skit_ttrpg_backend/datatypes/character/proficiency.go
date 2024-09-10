64a217755544c63998cbbbdee604d536ef0ae0a2
Xin Yang Liu <xliu324@UoA.auckland.ac.nz>
2024-09-10 18:45:17 +1200

    feat: Defined ENUMs for Proficiency
    As stated

--- skit_ttrpg_backend/datatypes/character/proficiency.go
+++ skit_ttrpg_backend/datatypes/character/proficiency.go
@@ -1,1 +1,45 @@
 package character
+
+type SkillProficiency int
+
+const{
+	Insight = iota
+	Society
+	Nature
+	Animal Handling
+	Persuasion
+	Deception
+	Performance
+	Intimidation
+	Arcane
+	MonsterAnatomy
+	Philosophy
+	Athletics
+	Acrobatics
+	MonsterAnatomy
+	Alchol Resistance
+	Technology
+}
+
+type CombatProficiency int
+
+const {
+	LightArmour
+	MediumArmour
+	HeavyArmour
+	MartialWeapons
+	SimpleWeapons
+	Will
+	Reflex
+	Fortitude
+}
+
+type ProficiencyType int
+
+const {
+	Untrained = iota
+	Trained
+	Expert
+	Master
+	Legendary
+}
